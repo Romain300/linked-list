08118e2dd29a479c4a72ab0f163d600843827a4d
Romain <[r.cappellotti@gmail.com]>
2024-11-17 22:04:31 +1100

    second commit

--- linkedList.js
+++ linkedList.js
@@ -3,24 +3,85 @@ class linkedList {
         this.list = list;
     };
 
+
     append(value) {
+        const newNode = new node(value);
         if(Object.keys(this.list).length === 0) {
-            this.list["head"] = value;
+            this.list["head"] = newNode;
             return;
         } else {
-            check(this.list["head"]);
+            checkTail(this.list["head"]);
         }
 
-        function check(data) {
-            if (data.nextNode === null) {
-                data.nextNode = value;
+        //checking where is the tail to add node at the end of the list
+        function checkTail(node) {
+            if (node.nextNode === null) {
+                node.nextNode = newNode;
+                return;
             } else {
-                check(data.nextNode);
+                checkTail(node.nextNode);
             };
         }
-
-
     };
+
+
+    prepend(value) {
+        const newNode = new node(value);
+        if(Object.keys(this.list).length === 0) {
+            this.list["head"] === newNode;
+        } else {
+            const currentHead = this.list["head"];
+            newNode.nextNode = currentHead;
+            this.list["head"] = newNode;
+        };
+    }
+
+
+    size() {
+        let i = 0;
+        if(Object.keys(this.list).length === 0) {
+            return 0;
+        } else {
+            checkEnd(this.list["head"]);
+            return i;
+            
+        };
+
+        //checking tail and i incrementation
+        function checkEnd(node) {
+            i++;
+            if (node.nextNode === null) {
+                return;
+            } else {
+                checkEnd(node.nextNode);
+            }; 
+        };
+    };
+
+
+    //Return node or value?
+    head() {
+        return this.list["head"];
+    };
+
+
+    //Return node or value?
+    tail() {
+        return findTail(this.list["head"]);
+        
+        function findTail(node) {
+            if(node.nextNode === null) {
+                return node;
+            } else {
+                return findTail(node.nextNode)
+            }
+        };
+    };
+
+
+
+
+
 
 }
 
@@ -32,10 +93,10 @@ class node {
     };
 };
 
-const newNode = new node();
-newNode.value = 10;
-console.log(newNode);
-const hello = new node(115)
+const newNode = "1st node";
+const hello = "2nd node"
+const yes = "3rd node";
+const okay = "4th node";
 
 
 
@@ -43,4 +104,9 @@ const hello = new node(115)
 const newList = new linkedList();
 newList.append(newNode);
 newList.append(hello)
+newList.append(yes)
+newList.prepend(okay)
 console.log(newList)
+console.log(newList.size())
+console.log(newList.head())
+console.log(newList.tail())
